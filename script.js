e8d4f108cef31c1d75aa6ee6bb962b84a852847f
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-07-31 19:12:09 +0100

    feat: Add hover grid effect to turn cells grey

--- script.js
+++ script.js
@@ -15,6 +15,26 @@ function createGrid(height, width) {
     }
 }
 
+function changeCellColour(cell, type) {
+    switch (type) {
+        case 'enter':
+            cell.style.backgroundColor = 'grey';
+            break;
+        case 'leave':
+            cell.style.backgroundColor = 'white';
+            break;
+    }
+}
+
+function addCellEventListeners(cell) {
+    console.log('test');
+    cell.addEventListener('mouseenter', () => changeCellColour(cell, 'enter'));
+    cell.addEventListener('mouseleave', () => changeCellColour(cell, 'leave'));
+}
+
 createGrid(16, 16);
 
 //set up event listners for hovering over cells
+let gridCells = document.querySelectorAll('.grid-cell');
+console.log('test');
+gridCells.forEach(cell => addCellEventListeners(cell));
