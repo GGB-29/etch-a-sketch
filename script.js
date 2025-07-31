f1dc835a5f06b1a6bc4746789ac35ac3560e0752
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-07-31 19:48:19 +0100

    feat: Add resize grid functionality

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-function createSquare(){
+function createSquare(cellSize){
     let container = document.querySelector('#grid-container');
 
     //create new grid cell
     const gridCell = document.createElement('div');
     gridCell.classList.add('grid-cell');
 
+    gridCell.style.width = cellSize + 'px';
+    gridCell.style.height = cellSize + 'px';
+
     container.appendChild(gridCell);
 }
 
 function createGrid(height, width) {
+    //calculate cell size
+    let gridSize = 800;
+    let cellSize = gridSize / height;
+
     //create grid of cells, height and width are in number of cells
     for (let i = 0; i < height * width; i++) {
-        createSquare();
+        createSquare(cellSize);
     }
 }
 
@@ -21,20 +28,44 @@ function changeCellColour(cell, type) {
             cell.style.backgroundColor = 'grey';
             break;
         case 'leave':
-            cell.style.backgroundColor = 'white';
+            cell.style.backgroundColor = 'light-grey';
             break;
     }
 }
 
 function addCellEventListeners(cell) {
-    console.log('test');
     cell.addEventListener('mouseenter', () => changeCellColour(cell, 'enter'));
     cell.addEventListener('mouseleave', () => changeCellColour(cell, 'leave'));
+}
+
+function getValidSize() {
+    let newSize = prompt('Enter new grid height: ');
+    while (!(newSize > 0) || !(newSize <= 100)) {
+        alert('Grid height must be between 1 and 100');
+        newSize = prompt('Enter new grid height: ');
+    }
+    return newSize;
+}
+
+function resize() {
+    const newSize = getValidSize();
+    let container = document.querySelector('#grid-container');
+    //clear all cells
+    container.innerHTML = '';
+
+    createGrid(newSize, newSize);
+
+    //set up event listners for hovering over cells
+    let gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(cell => addCellEventListeners(cell));
 }
 
 createGrid(16, 16);
 
 //set up event listners for hovering over cells
 let gridCells = document.querySelectorAll('.grid-cell');
-console.log('test');
 gridCells.forEach(cell => addCellEventListeners(cell));
+
+//set up button functionality
+let button = document.querySelector('#resize');
+button.addEventListener('click', () => resize());
